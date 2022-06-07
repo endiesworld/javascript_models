1a85f8ad3bff50d1c2f15f98e64a6764fa733162
endiesworld <endiesworld@gmail.com>
2022-06-07 15:43:20 +0100

    First commit

---
+++ javascript_values.js
@@ -0,0 +1,20 @@
+// Values and Code
+
+/*  
+    Values are diffrent from the codes(Instructions) we write. Codes interract with values
+*/
+
+// Primitive value type : Can't be created, destroyed or changed can only be pointed at 
+
+console.log(typeof(2))
+console.log(typeof('Emmanuel'))
+console.log(typeof(true))
+console.log(typeof(undefined))
+console.log(typeof(null)) // Prints an object value type, eventhough null is a primitive type.
+
+
+// Object or Functions : These are mutable values
+
+console.log(typeof({}))
+console.log(typeof([]))
+console.log(typeof(x => x**2))
