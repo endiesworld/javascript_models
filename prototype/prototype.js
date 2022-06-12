e10d303280c118a70c865ca48d43eaf3b8d28631
endiesworld <endiesworld@gmail.com>
2022-06-12 05:58:19 +0100

    prototype pollution

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -18,3 +18,16 @@ console.log(`Emmanuel has ${emmanuel.legs} legs`)
 console.log(`Does Emmanuel have the brainy property ? ${emmanuel.hasOwnProperty('brainy')}`)
 
 console.log(`Is Emmanuel bariny ? ${emmanuel.brainy}`)
+
+// When we read a property that doesn’t exist on our object, we’ll keep looking for it on the prototype chain.
+//  If we don’t find it, we get undefined.
+// But when we write a property that doesn’t exist on our object, that creates the property on our object
+
+let obj = {};
+console.log(obj.__proto__); 
+
+// prototype Poluting
+obj.__proto__.smell = 'banana';
+
+console.log(`What does Emmanue smell like ? ${emmanuel.smell}`)
+console.log(`Is Emmanuel bariny ? ${emmanuel.brainy}`)
