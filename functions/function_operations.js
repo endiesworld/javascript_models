b18993c9129f814e0a778d04d33aeaf653f1ea4c
Emmanuel <endiesworld@gmail.com>
2022-06-17 09:54:26 +0100

    function operations update from pc

--- functions/function_operations.js
+++ functions/function_operations.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Functions 
  */
@@ -56,3 +57,36 @@ let main_function = function ( x, y){
 let access = main_function(5, 6)
 
 console.log(access(9))
+=======
+// Function Declaration
+function squareroot(x){
+    // some other function statemants
+    return x*x
+}
+
+
+// Function Expression
+
+const cuberoot = function (x){
+    // Some other function statement
+    return x*x*x
+}
+
+
+function operational_fnc(opes, data){
+    let result = []
+    if (data instanceof Array ){
+        for(val of data){
+            result.push(opes(val))
+        }
+    }
+    else {
+        result = opes(data)
+    }
+    return result
+}
+
+const my_list = [2,4,6,8]
+
+console.log(operational_fnc(cuberoot, my_list))
+>>>>>>> e0359c8 (function operations)
