d8e776eadfe870d6c704af9c530a98c38a0bed4e
endiesworld <endiesworld@gmail.com>
2022-06-21 22:04:49 +0100

    new update

--- functions/function_operations.js
+++ functions/function_operations.js
@@ -56,3 +56,30 @@ let main_function = function ( x, y){
 let access = main_function(5, 6)
 
 console.log(access(9))
+
+// FUNTION PARAMETER
+/**
+ * REST PARAMETER
+ * Enables a function to treat all arguments like an array
+ * Also check the spread (...) operator, which is used to separate the elements of an array.
+ */
+
+const max = function(...values) {
+    console.log(values instanceof Array);
+    let large = values[0];
+    for(let i = 0; i < values.length; i++) {
+        if(values[i] > large) {
+            large = values[i];
+    }
+    }
+    return large;
+};
+console.log('*** REST PARAMETER*****')
+console.log(max(2, 1, 7, 4));
+console.log(max(4));
+console.log(max([9, 1, 7, 4, 11]));
+console.log(max(...[9, 1, 7, 4, 11]));
+
+let c = () => (1,23)
+let a , b = c()
+console.log(`a: ${a}, b: ${b}`)
