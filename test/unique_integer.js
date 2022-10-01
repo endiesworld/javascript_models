09404aa0408cfbc39618b34e66f3c60bf4d50bd8
Emmanuel <endiesworld@gmail.com>
2022-10-01 05:01:32 +0100

    Added array challenges

---
+++ test/unique_integer.js
@@ -0,0 +1,10 @@
+// Given an array, find the first integer, which is unique in the array. 
+// Unique means the number does not repeat and appears only once in the whole array.
+
+let arr = [9,2,3,2,6,6]
+
+function findFirstUnique(arr) {
+
+}
+
+console.log(findFirstUnique(arr))
